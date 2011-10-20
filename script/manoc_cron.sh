ff4eccf973a3ec3d40d590fa25830c429c4b99fc
rigorighetti <e.rigorighetti@gmail.com>
2011-10-20 15:52:00 +0200

    manoc_cron.sh bugfix

--- script/manoc_cron.sh
+++ script/manoc_cron.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-BASE_DIR=/home/manoc-stuff/manoc2
+BASE_DIR=
 LOG=$BASE_DIR/log/cron.log
 
 echo >> $LOG
 date >> $LOG
 perl $BASE_DIR/script/manoc_netwalker.pl >>$LOG 2>&1
 perl $BASE_DIR/script/manoc_archiver.pl  >>$LOG 2>&1
-#perl $BASE_DIR/script/manoc_backup.pl >>$LOG 2>&1
+perl $BASE_DIR/script/manoc_backup.pl >>$LOG 2>&1
