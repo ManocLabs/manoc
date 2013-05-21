6aeb44a3ce5e4b711145993d44fe536ff21d1c4f
Enrico Liguori <e.rigorighetti@gmail.com>
2013-05-21 12:06:34 +0200

    Introduced natural sorting in tables

--- root/static/js/dataTables.typeDetectPlugin.js
+++ root/static/js/dataTables.typeDetectPlugin.js
@@ -1,65 +1,8 @@
-jQuery.fn.dataTableExt.aTypes.push( function ( sData )
-{
-
-	sData = typeof sData.replace == 'function' ?
-		sData.replace( /<.*?>/g, "" ) : sData;
-	
-	if (/^\d{1,3}[\.]\d{1,3}[\.]\d{1,3}[\.]\d{1,3}$/.test(sData)) {
-	    return 'ip-html';
-	}
-	var sValidFirstChars = "0123456789-";
-	var sValidChars = "0123456789.";
-	var Char;
-	var bDecimal = false;
-	
-	/* Check for a valid first char (no period and allow negatives) */
-	Char = sData.charAt(0); 
-	if (sValidFirstChars.indexOf(Char) == -1) 
-	{
-		return null;
-	}
-	
-	/* Check all the other characters are valid */
-	for ( var i=1 ; i<sData.length ; i++ ) 
-	{
-		Char = sData.charAt(i); 
-		if (sValidChars.indexOf(Char) == -1) 
-		{
-			return null;
-		}
-		
-		/* Only allowed one decimal place... */
-		if ( Char == "." )
-		{
-			if ( bDecimal )
-			{
-				return null;
-			}
-			bDecimal = true;
-		}
-	}
-	
-	return 'num-html';
-} );
-
-jQuery.fn.dataTableExt.aTypes.push(
-	function ( sData )
-	{
-		if (/^\d{1,3}[\.]\d{1,3}[\.]\d{1,3}[\.]\d{1,3}$/.test(sData)) {
-			return 'ip-address';
-		}
-		return null;
-	}
-);
-
-jQuery.fn.dataTableExt.aTypes.push(
-	function ( sData )
-	{
-		if (/^<a\s+href=\"\S*\">\d{1,3}[\.]\d{1,3}[\.]\d{1,3}[\.]\d{1,3}<\/a>$/.test(sData)) {
-			return 'ip-html';
-		}
-		return null;
-	}
+jQuery.fn.dataTableExt.aTypes.unshift(
+        function ( sData )
+        {         
+            return "natural";
+        }
 );
 
 jQuery.fn.dataTableExt.aTypes.unshift(
--- root/static/js/dataTables.typeSortPlugin.js
+++ root/static/js/dataTables.typeSortPlugin.js
@@ -20,6 +20,43 @@ function trim(str) {
         return str;
 }
 
+function naturalSort (a, b) {
+    var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
+        sre = /(^[ ]*|[ ]*$)/g,
+        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
+        hre = /^0x[0-9a-f]+$/i,
+        ore = /^0/,
+        // convert all to strings and trim()
+        x = a.toString().replace(sre, '') || '',
+        y = b.toString().replace(sre, '') || '',
+        // chunk/tokenize
+        xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        // numeric, hex or date detection
+        xD = parseInt(x.match(hre)) || (xN.length != 1 && x.match(dre) && Date.parse(x)),
+        yD = parseInt(y.match(hre)) || xD && y.match(dre) && Date.parse(y) || null;
+    // first try and sort Hex codes or Dates
+    if (yD)
+        if ( xD < yD ) return -1;
+        else if ( xD > yD )  return 1;
+    // natural sorting through split numeric strings and default strings
+    for(var cLoc=0, numS=Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+        // find floats not starting with '0', string or 0 if not defined (Clint Priest)
+        var oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc]) || xN[cLoc] || 0;
+        var oFyNcL = !(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc]) || yN[cLoc] || 0;
+        // handle numeric vs string comparison - number < string - (Kyle Adams)
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) return (isNaN(oFxNcL)) ? 1 : -1;
+        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
+        else if (typeof oFxNcL !== typeof oFyNcL) {
+            oFxNcL += '';
+            oFyNcL += '';
+        }
+        if (oFxNcL < oFyNcL) return -1;
+        if (oFxNcL > oFyNcL) return 1;
+    }
+    return 0;
+}
+
 jQuery.extend( jQuery.fn.dataTableExt.oSort, {
   "date-italy-desc": function(x, y) {
     var xVal = getCustomEuroDateValue(x);
@@ -45,124 +82,14 @@ jQuery.extend( jQuery.fn.dataTableExt.oSort, {
         return 0;
 	  }
    },
-    "num-html-asc":  function(a,b) {
+    "natural-asc": function ( a, b ) {
 	var x = a.replace( /<.*?>/g, "" );
 	var y = b.replace( /<.*?>/g, "" );
-	x = parseFloat( x );
-	y = parseFloat( y );
-	return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+	return naturalSort(x,y);
     },
-    "num-html-desc":  function(a,b) {
+    "natural-desc": function ( a, b ) {
 	var x = a.replace( /<.*?>/g, "" );
 	var y = b.replace( /<.*?>/g, "" );
-	x = parseFloat( x );
-	y = parseFloat( y );
-	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
-    },
-    "ip-address-asc":  function(a,b) {
-	var m = a.split("."), x = "";
-	var n = b.split("."), y = "";
-	for(var i = 0; i < m.length; i++) {
-		var item = m[i];
-		if(item.length == 1) {
-			x += "00" + item;
-		} else if(item.length == 2) {
-			x += "0" + item;
-		} else {
-			x += item;
-		}
-	}
-	for(var i = 0; i < n.length; i++) {
-		var item = n[i];
-		if(item.length == 1) {
-			y += "00" + item;
-		} else if(item.length == 2) {
-			y += "0" + item;
-		} else {
-			y += item;
-		}
-	}
-	return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    },
-    'ip-address-desc':  function(a,b) {
-	var m = a.split("."), x = "";
-	var n = b.split("."), y = "";
-	for(var i = 0; i < m.length; i++) {
-		var item = m[i];
-		if(item.length == 1) {
-			x += "00" + item;
-		} else if (item.length == 2) {
-			x += "0" + item;
-		} else {
-			x += item;
-		}
-	}
-	for(var i = 0; i < n.length; i++) {
-		var item = n[i];
-		if(item.length == 1) {
-			y += "00" + item;
-		} else if (item.length == 2) {
-			y += "0" + item;
-		} else {
-			y += item;
-		}
-	}
-	return ((x < y) ? 1 : ((x > y) ? -1 : 0));
-    },
-    'ip-html-asc': function(a,b) {
-    var a1 = a.replace( /<.*?>/g, "" );
-    var b1 = b.replace( /<.*?>/g, "" );
-    
-    var m = a1.split("."), x = "";
-    var n = b1.split("."), y = "";
-    for(var i = 0; i < m.length; i++) {
-		var item = m[i];
-		if(item.length == 1) {
-		    x += "00" + item;
-		} else if(item.length == 2) {
-		    x += "0" + item;
-		} else {
-		    x += item;
-		}
+        return naturalSort(x,y) * -1;
     }
-    for(var i = 0; i < n.length; i++) {
-	var item = n[i];
-		if(item.length == 1) {
-		    y += "00" + item;
-		} else if(item.length == 2) {
-		    y += "0" + item;
-		} else {
-		    y += item;
-		}
-    }
-    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    },
-    'ip-html-desc':  function(a,b) {
-    var a1 = a.replace( /<.*?>/g, "" );
-    var b1 = b.replace( /<.*?>/g, "" );
-    
-    var m = a1.split("."), x = "";
-    var n = b1.split("."), y = "";
-	for(var i = 0; i < m.length; i++) {
-	    var item = m[i];
-	    if(item.length == 1) {
-		x += "00" + item;
-	    } else if (item.length == 2) {
-		x += "0" + item;
-	    } else {
-		x += item;
-	    }
-	}
-    for(var i = 0; i < n.length; i++) {
-	var item = n[i];
-	if(item.length == 1) {
-			y += "00" + item;
-	} else if (item.length == 2) {
-	    y += "0" + item;
-	} else {
-	    y += item;
-	}
-    }
-    return ((x < y) ? 1 : ((x > y) ? -1 : 0));
-    },
 });
