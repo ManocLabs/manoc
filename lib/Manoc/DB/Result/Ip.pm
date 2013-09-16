9505c3762981aece80df56a931bd93218628bf54
Enrico Liguori <e.rigorighetti@gmail.com>
2013-09-16 15:05:42 +0200

    IP schema class

---
+++ lib/Manoc/DB/Result/Ip.pm
@@ -0,0 +1,55 @@
+# Copyright 2011 by the Manoc Team
+#
+# This library is free software. You can redistribute it and/or modify
+# it under the same terms as Perl itself.
+package Manoc::DB::Result::Ip;
+use base 'DBIx::Class';
+use strict;
+use warnings;
+
+__PACKAGE__->load_components(qw/ Core InflateColumn/);
+__PACKAGE__->table('ip');
+
+__PACKAGE__->add_columns(
+    'ipaddr' => {
+        data_type   => 'varchar',
+        is_nullable => 0,
+        size        => 15
+    },
+    'description' => {
+        data_type   => 'text',
+        is_nullable => 1
+    },
+    'client' => {
+        data_type   => 'text',
+        is_nullable => 1
+    }, 
+    'contact' => {
+        data_type   => 'text',
+        is_nullable => 1
+    },
+    'phone' => {
+        data_type   => 'text',
+        is_nullable => 1
+    },
+    'email' => {
+        data_type   => 'text',
+        is_nullable => 1
+    },
+    'notes' => {
+        data_type   => 'text',
+        is_nullable => 1
+    },
+);
+
+__PACKAGE__->set_primary_key('ipaddr');
+
+__PACKAGE__->inflate_column(
+			    ipaddr => {
+				       inflate =>
+				       sub { return Manoc::IpAddress::Ipv4->new({ padded => $_[0] }) if defined($_[0]) },
+				       deflate => sub { return scalar $_[0]->padded if defined($_[0]) },
+				      } 
+			   );
+
+1;
