fcfa1b163cf2fa805b20611cc818b61ba90c3e73
Enrico Liguori <e.rigorighetti@gmail.com>
2014-01-23 17:01:21 +0100

    Row Groupinig Tables (part 3)

--- lib/Manoc/Controller/Device.pm
+++ lib/Manoc/Controller/Device.pm
@@ -115,7 +115,7 @@ sub view : Chained('object') : PathPart('view') : Args(0) {
                                ]});
   
     my @cdp_links;
-    my $time_group = 3600 * 12; #3 hours
+    my $time_group = $c->config->{Device}->{cdp_age} || 3600 * 12; #12 hours
     foreach my $n (@neighs){
       my $neigh_addr = $n->to_device; 
       $neigh_addr = $n->to_device->address if(ref $neigh_addr);
