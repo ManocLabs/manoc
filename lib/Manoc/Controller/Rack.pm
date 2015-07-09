c30338aa319ee947f103b48f3d5d90c907d0ae9a
Gabriele Mambrini <g.mambrini@gmail.com>
2015-07-09 19:13:00 +0200

    page layout review - updated Rack controller - updated print_* macros - moved section/subsection/title variables from template to page

--- lib/Manoc/Controller/Rack.pm
+++ lib/Manoc/Controller/Rack.pm
@@ -7,12 +7,25 @@ use strict;
 package Manoc::Controller::Rack;
 use Moose;
 use namespace::autoclean;
-use Data::Dumper;
 BEGIN { extends 'Catalyst::Controller'; }
+with 'Manoc::ControllerRole::CommonCRUD';
 with 'Manoc::ControllerRole::JSONView';
 
 use Manoc::Form::Rack;
 
+__PACKAGE__->config(
+    # define PathPart
+    action => {
+        setup => {
+            PathPart => 'rack',
+        }
+    },
+    class      => 'ManocDB::Rack',
+
+    create_page_title  => 'New rack',
+    edit_page_title    => 'Edit rack',
+    list_page_title    => 'Rack list',
+);
 
 =head1 NAME
 
@@ -26,162 +39,66 @@ Catalyst Controller.
 
 =cut
 
-=head2 index
+=head2 get_object_list
 
 =cut
 
-sub index : Path() : Args(0) {
-    my ( $self, $c ) = @_;
-    $c->response->redirect( $c->uri_for_action( 'rack/view' ) );
-    $c->detach();
-}
-
-=head2 base
-
-=cut
-
-sub base : Chained('/') : PathPart('rack') : CaptureArgs(0) {
-    my ( $self, $c ) = @_;
-    $c->stash( resultset => $c->model('ManocDB::Rack') );
-}
-
-=head2 object
-
-=cut
-
-sub object : Chained('base') : PathPart('id') : CaptureArgs(1) {
-
-    # $id = primary key
-    my ( $self, $c, $id ) = @_;
-
-    $c->stash( object => $c->stash->{resultset}->find($id) );
-
-    if ( !$c->stash->{object} ) {
-        $c->stash( error_msg => "Object $id not found!" );
-        $c->detach('/error/index');
-    }
-}
-
-=head2 view
-
-=cut
-
-sub view : Chained('object') : PathPart('view') : Args(0) {
-    my ( $self, $c ) = @_;
-    my $obj = $c->stash->{'object'};
-
-    $c->stash( template => 'rack/view.tt' );
-}
-
-=head2 list
-
-=cut
-
-sub fetch_list : Private {
+sub get_object_list {
     my ( $self, $c ) = @_;
 
-    $c->stash(object_list => [ $c->stash->{resultset}->search(
-        {},
+    return [ $c->stash->{resultset}->search(
+	{},
         {
             prefetch => 'building',
-            join     => 'building'
-        }) ]
-    );
+            join     => 'building',
+	    order_by => 'me.name',
+        })
+  ];
 }
-
-sub list : Chained('base') : PathPart('list') : Args(0) {
-    my ( $self, $c ) = @_;
-
-    $c->forward('fetch_list');
-    $c->stash( template   => 'rack/list.tt' );
-}
-
 
 =head2 create
 
 =cut
 
-sub create : Chained('base') : PathPart('create') : Args() {
+before 'create' => sub {
     my ( $self, $c) = @_;
 
     my $building_id = $c->req->query_parameters->{'building'};
     $c->log->debug("new rack in $building_id");
-    my $object = $c->stash->{resultset}->new_result({});
-    my $form = Manoc::Form::Rack->new(
-	item     => $object,
-    );
-
-    $c->stash( form => $form, template => 'rack/form.tt' );
-
-    my $success = $form->process(
-	params => $c->req->params,
-	defaults => { building => $building_id, name => 'ciao' },
-	use_defaults_over_obj => 1
-    );
-    if(!$success) {
-	return;
-    }
-
-    my $message = 'Rack ' . $c->req->param('name') . ' created.';
-    $c->flash( message => $message);
-    if ($c->stash->{is_xhr}) {
-        $c->stash(template => 'dialog/message.tt');
-	$c->stash(no_wrapper => 1);
-    }
-}
+    $c->stash(form_defaults => { building => $building_id });
+};
 
 
-=head2 edit
+=head2 get_form
 
 =cut
 
-sub edit : Chained('object') : PathPart('edit') : Args(0) {
+sub get_form {
     my ( $self, $c ) = @_;
-
-    my $item = $c->stash->{object};
-    my $form = Manoc::Form::Rack->new( item => $item );
-
-    $c->stash( form => $form, template => 'rack/form.tt' );
-    unless ( $form->process( params => $c->req->params ) ) {
-        return;
-    }
-
-    $c->flash( message => 'Rack modified ');
-    $c->response->redirect( $c->uri_for_action( '/rack/view', [ $c->stash->{object}->id ] ) );
-    $c->detach();
+    return Manoc::Form::Rack->new();
 }
 
 
-=head2 delete
+=head2 delete_object
 
 =cut
 
-sub delete : Chained('object') : PathPart('delete') : Args(0) {
+sub delete_object {
     my ( $self, $c ) = @_;
 
     my $rack = $c->stash->{'object'};
-    my $id   = $rack->id;
-    my $name = $rack->name;
 
-
-    if ( lc $c->req->method eq 'post' ) {
-        if ( $c->model('ManocDB::Device')->search( rack => $id )->count ) {
-            $c->flash( error_msg => "Rack is not empty. Cannot be deleted." );
-            $c->response->redirect( $c->uri_for_action( 'rack/view', [$id] ) );
-            $c->detach();
-        }
-
-        $rack->delete;
-        $c->flash( message => 'Rack ' . $name . '  deleted.' );
-	$c->response->redirect( $c->uri_for('/rack/list') );
-        $c->detach();
+    if ( $rack->devices->count ) {
+	$c->flash( error_msg => "Rack is not empty. Cannot be deleted." );
+	return undef;
     }
-    else {
-        $c->stash( template => 'generic_delete.tt' );
-    }
+
+     return $rack->delete;
 }
 
- 
+=head2 prepare_json_object
+
+=cut
 
 sub prepare_json_object : Private {
     my ($self, $rack) = @_;
