02f860f3c420f328c9e854ef6e9da0604956301f
Gabriele Mambrini <g.mambrini@gmail.com>
2015-08-21 20:07:49 +0200

    custom TT plugin path with json plugin

---
+++ lib/Manoc/View/TT/Plugin/JSON.pm
@@ -0,0 +1,53 @@
+# Copyright 2015 by the Manoc Team
+#
+# This library is free software. You can redistribute it and/or modify
+# it under the same terms as Perl itself.
+package Manoc::View::TT::Plugin::JSON;
+
+use strict;
+use warnings;
+
+use Template::Plugin;
+use base 'Template::Plugin';
+use JSON;
+
+
+=head1 NAME
+
+Manoc::View::TT::Plugin::JSON -  JSON Plugin for TT View
+
+=head1 DESCRIPTION
+
+JSON  utilities for Manoc TT View using L<JSON|JSON> module
+
+=cut
+
+
+sub json {
+	my $self = shift;
+	my $o = shift;
+	return to_json($o);
+}
+
+=head1 SEE ALSO
+
+L<Manoc>
+
+=head1 AUTHOR
+
+gabriele
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+# Local Variables:
+# mode: cperl
+# indent-tabs-mode: nil
+# cperl-indent-level: 4
+# cperl-indent-parens-as-block: t
+# End:
