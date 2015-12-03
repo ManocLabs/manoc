ec3823a350b32eee78add1097af8cbdbb928ee1c
Gabriele Mambrini <g.mambrini@gmail.com>
2015-12-03 21:15:03 +0100

    updated vagrant support

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@
 
 Vagrant.configure(2) do |config|
 
-  config.vm.box = "puppetlabs/centos-7.0-64-puppet"
+  config.vm.box = "centos/7"
 
   config.vm.network "forwarded_port", guest: 3000, host: 8080
 
--- maint/vagrant-setup.sh
+++ maint/vagrant-setup.sh
@@ -22,5 +22,23 @@ export PERL_CPANM_OPT
 
 echo "-----> Installing dependencies"
 echo "cpanm options: $PERL_CPANM_OPT"
-cd /vagrant && cpanm --installdeps . 2>&1
 
+( cd /vagrant && cpanm --installdeps . 2>&1 )
+
+
+echo "-----> Installing MIBS"
+MIBS_DIR="$INSTALL_DIR/netdisco-mibs"
+MIBS_TAR="$INSTALL_DIR/mibs.tar.gz"
+if ! [ -f "$MIBS_TAR" ]; then
+  NETDISCO_MIBS_URL="http://downloads.sourceforge.net/project/netdisco/netdisco-mibs/latest-snapshot/netdisco-mibs-snapshot.tar.gz"
+  curl -L "$NETDISCO_MIBS_URL" > "$MIBS_TAR"
+fi
+( cd "$INSTALL_DIR" && tar -xzf "$MIBS_TAR" )
+test -d /etc/snmp || mkdir -p /etc/snmp
+perl -ne "s|/usr/local/netdisco/mibs|$MIBS_DIR|; if ( /^mibdirs/ ) { /cisco|rfc|net-snmp/ and print } else { print }" "$MIBS_DIR/snmp.conf"> /etc/snmp/snmp.conf
+echo "Done"
+
+echo "-----> Setting firewall"
+firewall-cmd --get-default-zone | grep trusted || 
+	firewall-cmd --set-default-zone trusted
+echo "Done"
