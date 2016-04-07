4e1fd770a0c0075af77c5a3d5db6e7ed4ed48cb3
Gabriele Mambrini <g.mambrini@gmail.com>
2016-04-07 23:52:51 +0200

    mysql db support for vagrant box

--- maint/vagrant-setup.sh
+++ maint/vagrant-setup.sh
@@ -14,7 +14,7 @@ test -d "$INSTALL_DIR" || mkdir -p "$INSTALL_DIR"
 export PERL5LIB="$INSTALL_DIR/perl5"
 if ! [ -e /usr/local/bin/cpanm ]; then
   echo "-----> Bootstrapping cpanm"
-  curl -L --silent https://raw.github.com/miyagawa/cpanminus/master/cpanm | perl - App::cpanminus 2>&1 
+  curl -L --silent https://raw.github.com/miyagawa/cpanminus/master/cpanm | perl - App::cpanminus 2>&1
 fi
 
 PERL_CPANM_OPT="--quiet --notest -l $INSTALL_DIR \
@@ -45,10 +45,27 @@ echo "Done"
 
 echo "-----> Setting firewall"
 if firewall-cmd --state; then
-   firewall-cmd --get-default-zone | grep trusted || 
+   firewall-cmd --get-default-zone | grep trusted ||
 	firewall-cmd --set-default-zone trusted
 fi
 echo "Done"
 
 echo "-----> Setting profile"
-echo -e "eval \$(perl -Mlocal::lib=$INSTALL_DIR)" > /etc/profile.d/manoc.sh
+MANOC_SH_PROFILE=/etc/profile.d/manoc.sh
+MANOC_DB=manocdb
+cat <<EOF >$MANOC_SH_PROFILE
+eval \$(perl -Mlocal::lib=$INSTALL_DIR)
+export MANOC_DB_DSN="dbi:mysql:database=$MANOC_DB"
+export MANOC_DB_USERNAME=manoc_rw_user
+export MANOC_DB_PASSWORD=manoc123
+EOF
+. $MANOC_SH_PROFILE
+
+echo "-----> Setting MariaDB"
+yum install -y mariadb mariadb-server
+systemctl enable mariadb
+systemctl start mariadb
+if ! echo 'SHOW DATABASES'|mysql|grep $MANOC_DB>/dev/null; then
+  mysqladmin create $MANOC_DB
+fi
+echo "GRANT ALL ON ${MANOC_DB}.* TO '${MANOC_DB_USERNAME}'@'localhost' IDENTIFIED BY '${MANOC_DB_PASSWORD}'" | mysql
