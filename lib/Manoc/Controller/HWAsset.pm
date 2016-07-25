d72cc00e4423d60383b2a5f2b1478b882ab62a30
Gabriele Mambrini <g.mambrini@gmail.com>
2016-07-25 22:46:25 +0200

    update hwasset implementation

--- lib/Manoc/Controller/HWAsset.pm
+++ lib/Manoc/Controller/HWAsset.pm
@@ -10,7 +10,9 @@ use namespace::autoclean;
 BEGIN { extends 'Catalyst::Controller'; }
 with 'Manoc::ControllerRole::CommonCRUD';
 with 'Manoc::ControllerRole::JSONView';
+with 'Manoc::ControllerRole::JQDatatable';
 
+use Manoc::DB::Result::HWAsset;
 use Manoc::Form::HWAsset;
 
 =head1 NAME
@@ -33,8 +35,48 @@ __PACKAGE__->config(
     class      => 'ManocDB::HWAsset',
     form_class => 'Manoc::Form::HWAsset',
 
-    json_columns => [qw(serial vendor model inventory rack_id rack_level building room)],
+    json_columns => [ qw(serial vendor model inventory rack_id rack_level building room)],
+
+    datatable_row_callback => 'datatable_row',
 );
+
+=head1 METHODS
+
+=cut
+
+sub get_form_process_params {
+    my $self = shift;
+    my $c    = shift;
+
+    my %params = @_;
+
+    my $qp =  $c->req->query_parameters;
+
+    if ( my $type = $qp->{type} ) {
+        # check if is a valid type before using it
+        $Manoc::DB::Result::HWAsset::TYPE{$type} and
+            $params{preset_type} = $type;
+    }
+    $qp->{building} and $params{defaults}->{building} = $qp->{'building'};
+
+    return %params;
+}
+
+
+sub datatable_row {
+    my ($self, $c, $row) = @_;
+
+    return {
+        inventory => $row->inventory,
+        type      => $row->type,
+        vendor    => $row->vendor,
+        model     => $row->model,
+        serial    => $row->serial,
+        in_warehouse => $row->in_warehouse,
+        location  => $row->location,
+        link      => $c->uri_for_action('hwasset/view', [ $row->id ]),
+    }
+}
 
 =head1 AUTHOR
 
--- lib/Manoc/Form/HWAsset.pm
+++ lib/Manoc/Form/HWAsset.pm
@@ -6,11 +6,19 @@ with 'Manoc::Form::TraitFor::Horizontal';
 with 'Manoc::Form::TraitFor::SaveButton';
 with 'Manoc::Form::TraitFor::RackOptions';
 
+use Manoc::DB::Result::HWAsset;
+
 use namespace::autoclean;
 
 has '+item_class' => ( default => 'HWAsset' );
 has '+name'       => ( default => 'form-hwasset' );
 has '+html_prefix' => ( default => 1 );
+
+use constant {
+    LOCATION_WAREHOUSE => 'w',
+    LOCATION_RACK      => 'r',
+    LOCATION_SPECIFY   => 's',
+};
 
 sub build_render_list {
     [
@@ -18,7 +26,7 @@ sub build_render_list {
         'inventory',
         'vendor', 'model', 'serial',
         'location',
-        'building', 'location_block',
+        'location_block',
         'rack_block',
         'save',
         'csrf_token'
@@ -36,27 +44,27 @@ has_field 'inventory' => (
     type     => 'Text',
     size     => 32,
     required => 1,
-    label    => 'Inventory number',
+    label    => 'Inventory',
 );
 
 has_field 'vendor' => (
     type     => 'Text',
     size     => 32,
     required => 1,
-    label    => 'vendor',
+    label    => 'Vendor',
 );
 
 has_field 'model' => (
     type     => 'Text',
     size     => 32,
     required => 1,
-    label    => 'model',
+    label    => 'Model',
 );
 
 has_field 'serial' => (
     type  => 'Text',
     size  => 32,
-    label => 'Serial No',
+    label => 'Serial',
 );
 
 has_field 'location' => (
@@ -65,9 +73,9 @@ has_field 'location' => (
     label    => 'Location',
     widget   => 'RadioGroup',
     options  => [
-        { value => 'w', label => 'Warehouse' },
-        { value => 'r', label => 'Rack' },
-        { value => 's', label => 'Specify' },
+        { value => LOCATION_WAREHOUSE, label => 'Warehouse' },
+        { value => LOCATION_RACK,      label => 'Rack' },
+        { value => LOCATION_SPECIFY,   label => 'Specify' },
     ],
     wrapper_tags => { inline => 1 },
 );
@@ -84,7 +92,7 @@ has_field 'rack' => (
     type         => 'Select',
     label        => 'Rack',
     empty_select => '--- Select a rack ---',
-    required     => 1,
+    required     => 0,
 
     do_wrapper => 0,
     # we set wrapper=>0 so we don't have the inner div too!
@@ -107,28 +115,37 @@ has_field 'rack_level' => (
     label_class  => ['col-sm-2'],
 );
 
-has_field 'building' => (
-    type  => 'Select',
-    label => 'building',
-);
 
 has_block 'location_block' => (
-    render_list => [ 'room', 'floor' ],
+    render_list => [ 'building', 'room', 'floor' ],
     tag         => 'div',
     class       => ['form-group'],
 );
 
-has_field 'floor' => (
-    type  => 'Text',
-    label => 'Floor',
-    size  => 4,
-    label => 'floor',
+has_field 'building' => (
+    type         => 'Select',
+    empty_select => '---Choose a Building---',
+    required     => 0,
+    label        => 'Building',
     do_wrapper => 0,
     tags       => {
         before_element => '<div class="col-sm-4">',
         after_element  => '</div>'
     },
     label_class  => ['col-sm-2'],
+);
+
+
+has_field 'floor' => (
+    type  => 'Text',
+    label => 'Floor',
+    size  => 4,
+    do_wrapper => 0,
+    tags       => {
+        before_element => '<div class="col-sm-2">',
+        after_element  => '</div>'
+    },
+    label_class  => ['col-sm-1'],
 );
 
 has_field 'room' => (
@@ -137,13 +154,46 @@ has_field 'room' => (
     size  => 16,
     do_wrapper => 0,
     tags       => {
-        before_element => '<div class="col-sm-4">',
+        before_element => '<div class="col-sm-2">',
         after_element  => '</div>'
     },
-    label_class  => ['col-sm-2'],
+    label_class  => ['col-sm-1'],
 
 );
 
+
+sub default_location {
+    my $self = shift;
+    my $item = $self->item;
+
+    return unless $item;
+
+    $item->in_warehouse and return LOCATION_WAREHOUSE;
+    $item->rack and return LOCATION_RACK;
+    return LOCATION_SPECIFY;
+}
+
+sub options_building {
+    my $self = shift;
+    return unless $self->schema;
+    my @buildings =
+        $self->schema->resultset('Building')->search( {}, { order_by => 'name' } )->all();
+    my @selections;
+    foreach my $b (@buildings) {
+        my $option = {
+            label => $b->label,
+            value => $b->id
+        };
+        push @selections, $option;
+    }
+    return @selections;
+}
+
+sub options_rack {
+    my $self = shift;
+    return unless $self->schema;
+    return $self->get_rack_options;
+}
 
 sub options_type {
     my @results;
@@ -152,6 +202,38 @@ sub options_type {
     }
     return @results;
 }
+
+has 'preset_type' => ( is => 'rw', isa => 'Str' );
+
+before 'process' => sub {
+    my $self = shift;
+
+    my %args = @_;
+
+    if (my $type = $args{preset_type}) {
+        $self->preset_type($type);
+        #        $args{defaults}->{type} = $self->preset_type;
+        #        push @{ $args{inactive} }, 'type';
+        push @{ $self->inactive }, 'type';
+        $self->defaults->{type} = $type;
+    }
+};
+
+override 'update_model' => sub {
+    my $self   = shift;
+    my $values = $self->values;
+
+    $values->{location} eq LOCATION_WAREHOUSE and
+        $values->{in_warehouse} = 1;
+
+    if ( $self->preset_type ) {
+        $values->{type} = $self->preset_type;
+    }
+    $self->_set_value($values);
+
+    super();
+};
+
 
 __PACKAGE__->meta->make_immutable;
 no HTML::FormHandler::Moose;
