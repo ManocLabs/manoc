32f50ec8dc94fcd3c2b98d8f96df90626bfd0594
Gabriele Mambrini <g.mambrini@gmail.com>
2016-11-06 22:26:21 +0100

    first CSV import implementation (used by serverhw)

--- lib/Manoc/Controller/ServerHW.pm
+++ lib/Manoc/Controller/ServerHW.pm
@@ -7,6 +7,7 @@ use Moose;
 use namespace::autoclean;
 
 use Manoc::Form::ServerHW;
+use Manoc::Form::CSVImport::ServerHW;
 
 BEGIN { extends 'Catalyst::Controller'; }
 with "Manoc::ControllerRole::CommonCRUD";
@@ -40,6 +41,42 @@ __PACKAGE__->config(
     json_columns            => [ 'id', 'inventory', 'model', 'serial' ],
 );
 
+=head1 ACTIONS
+
+=head2 create
+
+Import a server hardware list from a CSV file
+
+=cut
+
+
+sub import_csv : Chained('base') : PathPart('importcsv') : Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->require_permission( 'serverhw', 'create' );
+    my $rs = $c->stash->{resultset};
+
+    my $upload;
+    $c->req->method eq 'POST' and $upload = $c->req->upload('file');
+
+
+    my $form = Manoc::Form::CVSImport::ServerHW->new(
+        ctx              => $c,
+        resultset        => $rs,
+    );
+    $c->stash->{form} = $form;
+
+    my %process_params;
+    $process_params{params} = $c->req->parameters;
+    $upload and $process_params{params}->{file} = $upload;
+    my $process_status = $form->process(%process_params);
+
+    return unless $process_status;
+
+    #$c->stash( message => $self->object_updated_message );
+    #$c->res->redirect( $self->get_form_success_url($c) );
+    #$c->detach();
+}
 
 =head1 AUTHOR
 
