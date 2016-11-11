94b9ece9a7aa096da502ea5f36433ed626b7d9c3
Gabriele Mambrini <g.mambrini@gmail.com>
2016-11-11 22:45:49 +0100

    support for differential ddl (use --diff)

--- script/manoc_create_ddl.pl
+++ script/manoc_create_ddl.pl
@@ -10,24 +10,10 @@ use Manoc::Support;
 package Manoc::DDLCreator;
 use Moose;
 
-use Manoc::DB;
 use SQL::Translator;
+use SQL::Translator::Diff;
 
-with 'MooseX::Getopt::Dashes';
-
-has 'debug' => (
-    is       => 'rw',
-    isa      => 'Bool',
-    required => 0,
-    default  => 0
-);
-
-has 'trace' => (
-    is       => 'rw',
-    isa      => 'Bool',
-    required => 0,
-    default  => 0
-);
+extends 'Manoc::Script';
 
 has 'comments' => (
     is       => 'rw',
@@ -57,14 +43,89 @@ has 'validate' => (
     default  => 0
 );
 
-has 'backend' => ( is => 'rw', isa => 'Str', required => 1 );
+has 'diff' => (
+    is       => 'rw',
+    isa      => 'Bool',
+    required => 0,
+    default  => 0
+);
+
+
+has 'backend' => (
+    is       => 'rw',
+    isa      => 'Str',
+    required => 0,
+    lazy     => 1,
+    builder  => '_build_backend',
+);
+
+sub _build_backend {
+    my $self = shift;
+
+    my $schema = $self->schema or return;
+    return $schema->storage->dbh->{Driver}->{Name};
+}
 
 sub run {
     my $self = shift;
 
+    $self->diff ?  $self->create_diff : $self->create_ddl;
+}
+
+sub create_diff {
+    my $self = shift;
+
+    my $dbh = $self->schema->storage->dbh;
+
+    my $be = $self->backend;
+    my $translator;
+
+    $translator = SQL::Translator->new(
+        debug          => $self->debug,
+        parser      => 'DBI',
+        parser_args => {
+            dbh => $dbh,
+        },
+    );
+    $translator->translate();
+    my $source_schema = $translator->schema or die SQL::Translator->error;;
+    $source_schema->name("Current");
+
+    $translator  =  SQL::Translator->new(
+        debug          => $self->debug,
+        parser      => 'SQL::Translator::Parser::DBIx::Class',
+        parser_args => { dbic_schema => $self->schema, },
+    );
+    $translator->producer($be);
+    my $expected_sql = $translator->translate();
+
+    $translator = SQL::Translator->new(
+        debug       => $self->debug,
+        parser      => $be,
+        data        => \$expected_sql,
+    );
+    $translator->translate();
+    my $target_schema = $translator->schema or die SQL::Translator->error;
+    $target_schema->name("New");
+
+    my $diff = SQL::Translator::Diff->new({
+        output_db     => $be,
+        source_schema => $source_schema,
+        target_schema => $target_schema,
+        debug         => $self->debug,
+
+        ignore_index_names => 1,
+        ignore_constraint_names => 1,
+    })->compute_differences->produce_diff_sql;
+
+    print $diff;
+}
+
+sub create_ddl {
+    my $self = shift;
+
     my $translator = SQL::Translator->new(
         debug          => $self->debug,
-        trace          => $self->trace,
         no_comments    => !$self->comments,
         show_warnings  => $self->show_warnings,
         add_drop_table => $self->add_drop_table,
@@ -75,7 +136,7 @@ sub run {
     $translator->parser('SQL::Translator::Parser::DBIx::Class');
 
     my $be = $self->backend;
-    $translator->producer("SQL::Translator::Producer::$be");
+    $translator->producer($be);
 
     my $output = $translator->translate() or die "Error: " . $translator->error;
 
