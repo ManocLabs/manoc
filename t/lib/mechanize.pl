45b510cffbcb65b9bba86aee80e341c9189f16f2
Gabriele Mambrini <g.mambrini@gmail.com>
2017-01-18 19:37:45 +0100

    mechanized tests

--- t/lib/mechanize.pl
+++ t/lib/mechanize.pl
@@ -1,13 +1,26 @@
+## Configure manoc via ENV
+
+# default login credentials for all testing code
+$ENV{MANOC_TEST_USER} = 'admin';
+$ENV{MANOC_TEST_PASS} = 'password';
+
+$ENV{MANOC_SKIP_CSRF} = 1;
+$ENV{MANOC_TEST_AUTOLOGIN} = 1;
+
+$ENV{MANOC_SUPPRESS_LOG} = 1
+    unless $ENV{NO_SUPPRESS_LOG};
+
+## Setup mech
+
 unless ( eval q{use Test::WWW::Mechanize::Catalyst 0.55; 1} ) {
     plan skip_all => 'Test::WWW::Mechanize::Catalyst >= 0.55 required';
     exit 0;
 }
 
-use vars qw/ $User $Pass $Mech /;
+use vars qw/ $Mech /;
 
 ok( $Mech = Test::WWW::Mechanize::Catalyst->new( catalyst_app => 'Manoc' ),
     "Created mech object" );
 
-# login credentials for all testing code
-our $User = 'admin';
-our $Pass = 'password';
+
+1;
---
+++ t/mech_search.t
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN {
+    use FindBin;
+    require "$FindBin::Bin/lib/inc.pl";
+    require "$FindBin::Bin/lib/mechanize.pl";
+}
+
+$Mech->get_ok( '/search' );
+$Mech->text_contains( 'Search' );
+
+done_testing();
