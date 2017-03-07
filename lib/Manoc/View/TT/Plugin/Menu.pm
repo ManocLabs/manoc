d7ed472d37fa3d5556163f88eb6726fa2136cb01
Gabriele Mambrini <g.mambrini@gmail.com>
2017-03-07 21:25:43 +0100

    bugfix: do not check permission when not authenticated

--- lib/Manoc/View/TT/Plugin/Menu.pm
+++ lib/Manoc/View/TT/Plugin/Menu.pm
@@ -209,7 +209,7 @@ sub _permission_check {
     my ( $c, $item ) = @_;
 
     $item->{permission} or return 1;
-    return $c->check_permission( $item->{permission} );
+    return $c->user && $c->check_permission( $item->{permission} );
 }
 
 =head1 SEE ALSO
