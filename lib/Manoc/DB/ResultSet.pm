06ec9598ffb9107a5e21111b094f600dae456c89
Gabriele Mambrini <g.mambrini@gmail.com>
2017-04-15 00:26:27 +0200

    fix for rs called in array context by HFH

--- lib/Manoc/DB/ResultSet.pm
+++ lib/Manoc/DB/ResultSet.pm
@@ -11,6 +11,16 @@ use parent 'DBIx::Class::ResultSet';
 
 __PACKAGE__->load_components('Helper::ResultSet');
 
+sub search {
+    # this is a terribile hack because HFH requires wantarray support
+    # for HTML::FormHandler::InitResult
+    (caller(1))[0] =~ /^HTML::FormHandler/ ||
+        (caller(2))[0] =~ /^HTML::FormHandler/ and
+        return wantarray ? shift->search(@_)->all : shift->search_rs(@_);
+
+    return shift->next::method(@_);
+}
+
 1;
 # Local Variables:
 # mode: cperl
