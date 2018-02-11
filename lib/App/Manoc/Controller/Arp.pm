b18504c2a6872eb651453a2839b50ef62d276565
Gabriele Mambrini <g.mambrini@gmail.com>
2018-02-11 17:27:24 +0100

    bugfix: fixed arp activity view for blocks and networks

--- lib/App/Manoc/Controller/Arp.pm
+++ lib/App/Manoc/Controller/Arp.pm
@@ -12,7 +12,29 @@ with
     'App::Manoc::ControllerRole::ResultSet',
     'App::Manoc::ControllerRole::JQDatatable';
 
-use App::Manoc::Utils::Datetime qw/print_timestamp/;
+use App::Manoc::Utils::Datetime qw(print_timestamp str2seconds);
+
+__PACKAGE__->config(
+    # define PathPart
+    action => {
+        setup => {
+            PathPart => 'arp',
+        }
+    },
+    class => 'ManocDB::Arp',
+
+    datatable_row_callback => sub {
+        my ( $self, $c, $row ) = @_;
+        my $address = App::Manoc::IPAddress::IPv4->new( $row->get_column('ipaddr') );
+        return [
+            "$address",
+            print_timestamp( $row->get_column('firstseen') ),
+            print_timestamp( $row->get_column('lastseen') ),
+        ];
+    },
+    datatable_search_columns => [qw/ipaddr/],
+    datatable_columns        => [qw/ipaddr firstseen lastseen/]
+);
 
 =head1 DESCRIPTION
 
@@ -24,40 +46,61 @@ Catalyst Controller.
 
 =cut
 
-sub list : Private {
+sub list : Chained('base') : PathPart('') : Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->require_permission( 'arp', 'view' );
+    if ( my $days = $c->req->params->{days} ) {
+        $c->stash( days => int($days) );
+    }
 
-    $c->stash( template => 'arp/list.tt' );
+    my $network_id = $c->req->params->{ipnetwork};
+    if ( defined($network_id) ) {
+        my $network = $c->model('ManocDB::IPNetwork')->find($network_id);
+
+        # used in template
+        $network and $c->stash( ipnetwork => $network );
+    }
+
+    my $block_id = $c->req->params->{ipblock};
+    if ( defined($block_id) ) {
+        my $ipblock = $c->model('ManocDB::IPBlock')->find($block_id);
+
+        # used in template
+        $ipblock and $c->stash( ipblock => $ipblock );
+    }
 }
 
-=action list_js
+=method get_datatable_resultset
 
 =cut
 
-sub list_js : Private {
+sub get_datatable_resultset {
     my ( $self, $c ) = @_;
 
-    $c->require_permission( 'arp', 'view' );
+    my $rs = $c->stash->{resultset};
 
-    my $row_callback = sub {
-        my ( $ctx, $row ) = @_;
-        my $address = App::Manoc::IPAddress::IPv4->new( $row->get_column('ip_address') );
-        return [
-            "$address",
-            print_timestamp( $row->get_column('firstseen') ),
-            print_timestamp( $row->get_column('lastseen') ),
-        ];
-    };
+    my $network_id = $c->req->params->{ipnetwork};
+    if ( defined($network_id) ) {
+        my $network = $c->model('ManocDB::IPNetwork')->find($network_id);
 
-    $c->stash(
-        datatable_row_callback   => $row_callback,
-        datatable_search_columns => [qw/ipaddr/],
-        datatable_columns        => [qw/ipaddr firstseen lastseen/],
-    );
+        $c->debug and $c->log->debug("Using network $network_id for filtering ARP");
+        $network and $rs = $network->arp_entries;
+    }
 
-    $c->detach('datatable_response');
+    my $block_id = $c->req->params->{ipblock};
+    if ( defined($block_id) ) {
+        my $block = $c->model('ManocDB::IPBlock')->find($block_id);
+
+        $c->debug and $c->log->debug("Using block $block_id for filtering ARP");
+        $block and $rs = $block->arp_entries;
+    }
+
+    my $days = $c->req->params->{days};
+    if ( $days && int($days) ) {
+        $rs = $rs->search( { lastseen => { '>=' => time - str2seconds( int($days), 'd' ) } } );
+    }
+
+    return $rs;
 }
 
 __PACKAGE__->meta->make_immutable;
